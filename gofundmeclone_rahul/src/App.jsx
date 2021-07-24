71f8eb5f40169e4774c50804511e70286ceba31c
Digvijay <digvijay30786@gmail.com>
2021-07-24 21:37:00 +0530

    Add payment Getway

--- gofundmeclone_rahul/src/App.jsx
+++ gofundmeclone_rahul/src/App.jsx
@@ -1,5 +1,7 @@
 import Main from "./components/components/main";
-import Works from './components/components/works'
+import Works from './components/components/works';
+import Donate from './components/components/donate';
+import Demo from './components/components/demo';
 import Rahul from './components/rahul';
 import {Switch,Route} from "react-router-dom";
 export default function App() {
@@ -9,6 +11,8 @@ export default function App() {
       <Route exact path='/' component={Main} />
       <Route exact path='/works' component={Works} />
       <Route path='/rahul' component={Rahul} />
+      <Route path='/donate' component={Donate} />
+      <Route path='/demo' component={Demo} />
     </Switch>
   );
 }
